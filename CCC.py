17ad73866dbce9dca2439ad8113b748b6a4f70c7
sergey <a89397074218@gmail.com>
2024-09-30 07:34:09 +0300

    added GBR currency

--- CCC.py
+++ CCC.py
@@ -15,41 +15,51 @@ def make_api_request():
 
     url_usd = 'https://ru.investing.com/currencies/usd-rub'
     url_eur = 'https://ru.investing.com/currencies/eur-rub'
+    url_gbr = 'https://ru.investing.com/currencies/gbp-rub'
 
     text_usd = 'text-5xl/9 font-bold text-[#232526] md:text-[42px] md:leading-[60px]'
-    text_usd_growth = 'flex items-center gap-2 text-base/6 font-bold md:text-xl/7 rtl:force-ltr text-positive-main'
-    text_eur = ('text-5xl/9 font-bold text-[#232526] md:text-[42px] md:leading-[60px]')
-    text_eur_growth = 'flex items-center gap-2 text-base/6 font-bold md:text-xl/7 rtl:force-ltr text-positive-main'
+    text_usd_growth = 'flex items-center gap-2 text-base/6 font-bold md:text-xl/7 rtl:force-ltr text-negative-main'
+    text_eur = 'text-5xl/9 font-bold text-[#232526] md:text-[42px] md:leading-[60px]'
+    text_eur_growth = 'flex items-center gap-2 text-base/6 font-bold md:text-xl/7 rtl:force-ltr text-inv-grey-700'
+    text_gbr = 'text-5xl/9 font-bold text-[#232526] md:text-[42px] md:leading-[60px]'
+    text_gbr_growth = 'flex items-center gap-2 text-base/6 font-bold md:text-xl/7 rtl:force-ltr text-positive-main'
 
     #отправка запроса валюты
     response_usd = rq.get(url_usd)
     response_eur = rq.get(url_eur)
+    response_gbr = rq.get(url_gbr)
 
     requests_on_usd = BS(response_usd.text, features='html.parser')
     requests_on_eur = BS(response_eur.text, features='html.parser')
+    requests_on_gbr = BS(response_gbr.text, features='html.parser')
 
     text_usd = requests_on_usd.find(class_=text_usd)
     text_eur = requests_on_eur.find(class_=text_eur)
+    text_gbr = requests_on_gbr.find(class_=text_gbr)
 
     #отправка запроса роста валюты
     response_usd_growth = rq.get(url_usd)
     response_eur_growth = rq.get(url_eur)
+    response_gbr_growth = rq.get(url_gbr)
 
     requests_on_usd_growth = BS(response_usd_growth.text, features='html.parser')
     requests_on_eur_growth = BS(response_eur_growth.text, features='html.parser')
+    requests_on_gbr_growth = BS(response_gbr_growth.text, features='html.parser')
 
     text_usd_growth = requests_on_usd_growth.find(class_=text_usd_growth)
     text_eur_growth = requests_on_eur_growth.find(class_=text_eur_growth)
+    text_gbr_growth = requests_on_gbr_growth.find(class_=text_gbr_growth)
 
     #валюта
     USD_y = text_usd.text
     EUR_y = text_eur.text
+    GBR_y = text_gbr.text
 
-    name_text_one = ['Крипта:']
     name_text_two = ['Валюта:']
 
     data = ({'USD': [USD_y],
-           'EUR': [EUR_y]})
+             'EUR': [EUR_y],
+             'GBR': [GBR_y]})
 
 
     base_data_one = pd.DataFrame(data=data, index=[name_text_two])
@@ -59,11 +69,13 @@ def make_api_request():
     #рост валюты
     USD_y_growth = text_usd_growth.text
     EUR_y_growth = text_eur_growth.text
+    GBR_y_growth = text_gbr_growth.text
 
     name_text_two_growth = ['Рост:']
 
     data = ({'USD-GROWTH': [USD_y_growth],
-           'EUR-GROWTH': [EUR_y_growth]})
+             'EUR-GROWTH': [EUR_y_growth],
+             'OIL-GROWTH': [GBR_y_growth]})
 
 
     base_data_two = pd.DataFrame(data=data, index=[name_text_two_growth])
@@ -74,6 +86,7 @@ def make_api_request():
 
     USD_y = int(float(USD_y.replace(',','.')))
     EUR_y = int(float(EUR_y.replace(',','.')))
+    GBR_y = int(float(GBR_y.replace(',','.')))
 
     fig = plt.figure(figsize=(7,4))
     ax = plt.subplot()
@@ -84,15 +97,19 @@ def make_api_request():
     x2 = np.array([2])
     y2 = np.array([EUR_y])
 
+    x3 = np.array([3])
+    y3 = np.array([GBR_y])
+
     ax.bar(x1,y1,label='USD')
     ax.bar(x2,y2,label='EUR')
+    ax.bar(x3, y3, label='GBR')
 
-    ax.xaxis.set_major_locator(LinearLocator(2))
-    ax.xaxis.set_major_formatter(FixedFormatter([USD_y_growth,EUR_y_growth]))
+    ax.xaxis.set_major_locator(LinearLocator(3))
+    ax.xaxis.set_major_formatter(FixedFormatter([USD_y_growth,EUR_y_growth,GBR_y_growth]))
 
-    fig.suptitle('It is monitoring USD/EUR')
+    fig.suptitle('It is monitoring USD/EUR/GBR')
 
-    ax.legend(['USD','EUR'])
+    ax.legend(['USD','EUR','GBR'])
 
     plt.show()
     plt.close(fig)
